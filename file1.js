76535b439fd5a532aa33d120343bfaf40e045519
ruchita <ruchitasagalgile04@gmail.com>
2023-11-02 06:13:35 +0530

    data types in js

---
+++ file1.js
@@ -0,0 +1,59 @@
+// Data types in js
+
+/*
+Numbers,
+String,
+Booleans
+Undefined
+NUll
+Arrays
+Objects
+
+Maps
+sets
+
+Not a typed language
+
+typescript is superset of 
+
+
+ */
+let x = 2e7;
+console.log(x);
+
+let firstName = "mohit";
+let lastName = "sa";
+
+console.log(`${firstName} ${lastName}`);
+
+let y = true;
+console.log(y);
+
+//implicit corcion: meaning whenever u give wierd datatype :
+// it implicitly made some calculation
+//should give error
+console.log(2 + "mohit");
+//implicit
+
+console.log(2 + "mohit" + NaN);
+
+//any positive number which is not 0 is consider as truth value
+// any 0 or negative number is consider as false value
+
+//true 1,34,24,55
+//false 0,-3,-4
+
+//that variable is available in  the memory but it has value as null
+let u = null;
+
+//try to find something in memory but its value is not in the memory not in the memory
+
+// undefined =not defined
+let h = null;
+console.log(h);
+
+//undefined= varibale present, value absent
+//null: variable present, initialized with null
+
+//maps :are performant objects
+// sets : strict arrays
