f2106b058a667a7daa05b42522da53d9a6289de9
ruchita <ruchitasagalgile04@gmail.com>
2023-11-02 21:40:17 +0530

    global scope vs functional scope

--- file2.js
+++ file2.js
@@ -13,20 +13,49 @@
 // very imp :var is functionsl scope
 
 //let are local block level scope
-function sayHello() {
-  let y = 2;
-  if (true) {
-    let y = 3;
-    console.log(y);
-  }
-  console.log(y);
-}
-sayHello();
+// function sayHello() {
+//   let y = 2;
+//   if (true) {
+//     let y = 3;
+//     console.log(y);
+//   }
+//   console.log(y);
+// }
+// sayHello();
 
 //with var let we can reinitilaization of value
 
-const x = 2;
-x = 3;
-console.log(x);
+// const x = 2;
+// x = 3;
+// console.log(x);
 
 //const we can not reinitiazaed value
+
+// question:  what is the type of error i will get
+// ReferenceError;
+
+// when we get type error : when the datatype doesnt match
+// ReferenceError : when the js is trying to access something which is not available in the memory
+
+// var y = 3; //globally scoped
+// function abc() {
+//   console.log(y);
+//   var x = 3;
+// }
+// console.log(x);
+
+var x = 2;
+var isAvailable = 3;
+if (isAvailable >= 3) {
+  var x = 3;
+}
+console.log(x);
+// output:2
+// cannot redeclare using let but reinitialize
+
+//const cannot leave const empty
+
+let x;
+console.log(x);
+
+x = 3;
